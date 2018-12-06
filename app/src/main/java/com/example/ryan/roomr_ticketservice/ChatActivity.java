da013f19f13d88227ae804c05c8adb2b6eb789bd
Dragan Kovacevic <dragsterd@gmail.com>
2018-12-06 15:51:08 -0500

    Added chat to certain tenants to chat activity

--- app/src/main/java/com/example/ryan/roomr_ticketservice/ChatActivity.java
+++ app/src/main/java/com/example/ryan/roomr_ticketservice/ChatActivity.java
@@ -47,11 +47,12 @@ public class ChatActivity extends AppCompatActivity implements RoomListener {
         ten2 = findViewById(R.id.tenant2);
         ten3 = findViewById(R.id.tenant3);
 
-        ten1.setText("Mike Rose");
+        ten1.setText("667 Sarah Crescent");
         ten2.setText("Ryan Sneyd");
         ten3.setText("Rodrigo Hurtado");
 
-        ten2.setOnClickListener(onSelectTenant);
+        ten2.setOnClickListener(onSelectTenant2);
+        ten1.setOnClickListener(onSelectTenant1);
 
 
 
@@ -126,8 +127,8 @@ public class ChatActivity extends AppCompatActivity implements RoomListener {
     }
 
     private String getRandomName() {
-        String[] adjs = {"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"};
-        String[] nouns = {"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"};
+        String[] adjs = {"Ryan"};
+        String[] nouns = {"Sneyd"};
         return (
                 adjs[(int) Math.floor(Math.random() * adjs.length)] +
                         "_" +
@@ -136,15 +137,32 @@ public class ChatActivity extends AppCompatActivity implements RoomListener {
     }
 
     //when a tenant is clicked
-    private View.OnClickListener onSelectTenant = new View.OnClickListener() {
+    private View.OnClickListener onSelectTenant1 = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent i = getIntent();
+
+
+            channelID = "4lmrrOD8Ll2SkO2A";
+
+            startActivity(i);
+
+        }
+    };
+    //when a tenant is clicked
+    private View.OnClickListener onSelectTenant2 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = getIntent();
+
+
             channelID = "FGcdTGfNcZHXPRIA";
 
             startActivity(i);
+
         }
     };
+
 
     private String getRandomColor() {
         Random r = new Random();
